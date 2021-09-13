4db281dc556354cf2fa02a9915044b30a8111516
zenith-noble01 <webdev686@gmail.com>
2021-09-13 03:21:30 +0100

    giving it's basic template

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     zenith with ssams the landing page
+    </div>
+  );
+}
+
+export default App;
